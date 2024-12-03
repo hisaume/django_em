304ad878e5729807c6b1c0d25de7a1252c0db90d
hisaume <hisaume@proton.me>
2024-12-03 22:38:31 +0000

    First Project model, register and DB migration

--- django_em/settings.py
+++ django_em/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'core',
+    'projects',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +120,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# ??????? WHY 2 separate folders for 'static'? ???????
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+   title = models.CharField(max_length=200)
+   description = models.TextField()
+   technology = models.CharField(max_length=200)
+   image = models.ImageField(upload_to='projects/')
+   github_url = models.URLField(blank=True)
+   live_url = models.URLField(blank=True)
+   created_at = models.DateTimeField(auto_now_add=True)
+
+   def __str__(self):
+       return self.title
+
+   class Meta:
+       ordering = ['-created_at']
