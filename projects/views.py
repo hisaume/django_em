b05148eece9656bf7d4bd082d47b1aac5fac59ef
hisaume <hisaume@proton.me>
2024-12-05 16:21:45 +0000

    Added views.py in core and porjects folders

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
+def project_list(request):
+    projects = Project.objects.all()
+    return render(request, 'projects/project_list.html', {'projects':projects})
+
+def project_detail(request):
+    project = Project.objects.all()
+    return render(request, 'projects/project_detail.html', {'project': project})
