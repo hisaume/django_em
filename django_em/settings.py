aeb76b635177fab610e7cb9d6a6981b7414e0ce9
hisaume <hisaume@proton.me>
2024-12-06 22:48:58 +0000

    Templates path set. Templates correctly located.

--- django_em/settings.py
+++ django_em/settings.py
@@ -57,7 +57,8 @@ ROOT_URLCONF = 'django_em.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [], # BASE_DIR / 'templates'
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
+                    # BASE_DIR / 'templates'
                     # os.path.join(BASE_DIR, 'templates')
         'APP_DIRS': True,
         'OPTIONS': {
