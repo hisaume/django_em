c94df16ca41a3f7b47e405f6d5d47cd9329c36c4
hisaume <hisaume@proton.me>
2024-12-07 20:27:27 +0000

    project_detail def and settings.py corrected.

--- projects/views.py
+++ projects/views.py
@@ -6,6 +6,6 @@ def project_list(request):
     projects = Project.objects.all()
     return render(request, 'projects/project_list.html', {'projects':projects})
 
-def project_detail(request):
-    project = Project.objects.all()
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
     return render(request, 'projects/project_detail.html', {'project': project})
